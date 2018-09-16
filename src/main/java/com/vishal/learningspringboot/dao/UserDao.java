784eaa1615a511e2ee8b135511283c9374d44616
vishaladdala <31286487+vishaladdala@users.noreply.github.com>
2018-09-15 21:27:51 -0500

    First

---
+++ src/main/java/com/vishal/learningspringboot/dao/UserDao.java
@@ -0,0 +1,24 @@
+package com.vishal.learningspringboot.dao;
+
+import java.util.List;
+import java.util.UUID;
+
+
+
+import com.vishal.learningspringboot.model.User;
+
+public interface UserDao {
+	
+	List<User> getAllUsers();
+	
+	User getUser(UUID userUid);
+	
+	int updateUser(User user);
+	
+	int removeUser(UUID userUid);
+	
+	int insertUser(User user);
+	
+	
+
+}
